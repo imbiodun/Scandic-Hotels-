163ff04edb69f0144d9b570a9ab10494f9e08125
Andrew6201 <okekechiemerie2021@gmail.com>
2026-05-15 13:19:43 +0300

    some settings corrections

--- ScandicHotel/ScandicHotel/settings.py
+++ ScandicHotel/ScandicHotel/settings.py
@@ -35,7 +35,7 @@ DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
