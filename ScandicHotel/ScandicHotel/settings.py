9318b961c5c24952f49988624fb6831be450288f
imbiodun <akinrinolaabiodun16@gmail.com>
2026-05-15 18:11:20 +0300

    Allowed Hosts

--- ScandicHotel/ScandicHotel/settings.py
+++ ScandicHotel/ScandicHotel/settings.py
@@ -21,7 +21,7 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1,scandic-hotel-website.onrender.com"
+    "localhost,127.0.0.1,scandic-hotels.onrender.com"
 ).split(",")
 
 
